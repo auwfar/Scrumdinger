411ba6d6992571543c0c603267078516526a5283
Auwfar <achmad.chadilauwfar@gmail.com>
2025-03-17 14:30:00 +0700

    Add a theme picker

--- Scrumdinger/Models/Theme.swift
+++ Scrumdinger/Models/Theme.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-public enum Theme: String {
+enum Theme: String, CaseIterable, Identifiable {
     case bubblegum
     case buttercup
     case indigo
@@ -36,5 +36,8 @@ public enum Theme: String {
     }
     var name: String {
         rawValue.capitalized
+    }
+    var id: String {
+        name
     }
 }
--- Scrumdinger/ThemeView.swift
+++ Scrumdinger/ThemeView.swift
@@ -11,7 +11,7 @@ struct ThemeView: View {
     let theme: Theme
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(theme.name)
             .padding(4)
             .frame(maxWidth: .infinity)
             .background(theme.mainColor)
